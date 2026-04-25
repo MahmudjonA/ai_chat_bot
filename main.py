c2095e1f33b18602f068caa709881d977c68d26e
MahmudjonA <abdumuratov1991@gmail.com>
2026-04-25 23:46:20 +0500

    add ci cd

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware  # 👈 ДОБАВЬ
+
 from routers import upload, chat
 
 app = FastAPI(title="AI FAQ Backend")
+
+# 🔥 CORS НАСТРОЙКА (ВОТ СЮДА ДОБАВЛЯЕТСЯ)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # пока открываем для всех
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(upload.router)
 app.include_router(chat.router)
